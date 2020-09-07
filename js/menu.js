dfe7d4f25198b42f1d46d1ad4482b58eed607df6
dannyvaneeckhoutte <60157535+dannyvaneeckhoutte@users.noreply.github.com>
2020-09-07 11:48:22 +0200

    moved JS code to JS seperate file

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+// animate hamburger menu
+// Look for .hamburger
+var hamburger = document.querySelector('.hamburger');
+// On click
+hamburger.addEventListener('click', function () {
+	// Toggle class "is-active"
+	hamburger.classList.toggle('is-active');
+	// Do something else, like open/close menu
+});
+
+// Show/hide menu
+function showMenu() {
+	var x = document.getElementById('myLinks');
+	if (x.style.display === 'block') {
+		x.style.display = 'none';
+	} else {
+		x.style.display = 'block';
+	}
+}
