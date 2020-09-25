1011a301c7b85f34a149356bca55460868a6727e
dannyvaneeckhoutte <60157535+dannyvaneeckhoutte@users.noreply.github.com>
2020-09-25 08:32:40 +0200

    accepted inc changes

--- create.php
+++ create.php
@@ -22,81 +22,76 @@ if (isset ($_POST['categorie'])  && isset($_POST['gerecht'])  && isset($_POST['p
 <?php 
    session_start();
    if(isset($_SESSION['sessie_gebr_id']) && $_SESSION['sessie_gebr_id'] != "") {
-<<<<<<< HEAD
     echo '<div class="logout-wrapper" style="position:relative; text-align:center; padding: 0.5em">';
     echo '<h4>Welkom, '.$_SESSION['sessie_gebruiker'].'</h4>';
     echo '<a style="color: white"href="logout.php">Logout</a>';
     echo '</div>';
-=======
       echo '<div class="logout-wrapper" style="position:relative; text-align:right; padding: 0.5em">';
       echo '<h4>Welcome '.$_SESSION['sessie_gebruiker'].'</h4>';
       echo '<a style="color: white"href="logout.php">Logout</a>';
       echo '</div>';
->>>>>>> 6427720319898bebc9a1472f55b4a58514f6f1aa
 
       } else { 
          header("Refresh:1; url=login.php");
       }
 ?>
 <div class="container">
-<<<<<<< HEAD
-  <div class="card mt-5">
-    <div class="card-header">
-      <h2>Gerecht toevoegen</h2>
-    </div>
-    <div class="card-body">
-
-      <?php if(!empty($message)): ?>
-      <div class="alert alert-success">
-        <?= $message; ?>
+   <div class="card mt-5">
+      <div class="card-header">
+         <h2>Gerecht toevoegen</h2>
       </div>
-      <?php endif; ?>
+      <div class="card-body">
 
-      <form method="post">
-        <div class="from-group">
-          <label for="categorie">Categorie</label>
-          <br>
-          <select name="categorie" id="categorie" class="mb-3" required>
-            <option value="">-- selecteer</option>
-            <option value="ontbijt">eat-Ontbijt</option>
-            <option value="lunch">eat-Lunch</option>
-            <option value="kids">eat-Kids</option>
-            <option value="zoet">eat-Zoet</option>
-            <option value="warm">drink-Warm</option>
-            <option value="fris">drink-Fris</option>
-            <option value="bier">drink-Bier</option>
-            <option value="geestrijk">drink-Geestrijk</option>
-            <option value="bubbels">drink-Bubbels</option>
-            <option value="rood">drink-Rood</option>
-            <option value="wit">drink-Wit</option>
-            <option value="rose">drink-Rosé</option>
-          </select>
+         <?php if(!empty($message)): ?>
+         <div class="alert alert-success">
+            <?= $message; ?>
+         </div>
+         <?php endif; ?>
 
-        </div>
-        <div class="form-group">
-          <label for="gerecht">Gerecht</label>
-          <input type="text" name="gerecht" id="name" class="form-control" required>
-        </div>
-        <div class="form-group">
-          <label for="prijs">Prijs</label>
-          <input type="number" name="prijs" id="prijs" class="form-control" min="0" max="99" step=".01" required>
-        </div>
-        <div class="form-group">
-          <fieldset>
-            <legend>Beschikbaar als takeaway?</em></legend>
-            <input type="radio" name="actief" value="1" />
-            <label for="actief">Ja</label>
-            <input type="radio" name="actief" value="0" />
-            <label for="actief">Nee</label>
-          </fieldset>
-        </div>
-        <div class="form-group">
-          <button type="submit" class="btn btn-info">Toevoegen</button>
-        </div>
-      </form>
-    </div>
-  </div>
-=======
+         <form method="post">
+            <div class="from-group">
+               <label for="categorie">Categorie</label>
+               <br>
+               <select name="categorie" id="categorie" class="mb-3" required>
+                  <option value="">-- selecteer</option>
+                  <option value="ontbijt">eat-Ontbijt</option>
+                  <option value="lunch">eat-Lunch</option>
+                  <option value="kids">eat-Kids</option>
+                  <option value="zoet">eat-Zoet</option>
+                  <option value="warm">drink-Warm</option>
+                  <option value="fris">drink-Fris</option>
+                  <option value="bier">drink-Bier</option>
+                  <option value="geestrijk">drink-Geestrijk</option>
+                  <option value="bubbels">drink-Bubbels</option>
+                  <option value="rood">drink-Rood</option>
+                  <option value="wit">drink-Wit</option>
+                  <option value="rose">drink-Rosé</option>
+               </select>
+
+            </div>
+            <div class="form-group">
+               <label for="gerecht">Gerecht</label>
+               <input type="text" name="gerecht" id="name" class="form-control" required>
+            </div>
+            <div class="form-group">
+               <label for="prijs">Prijs</label>
+               <input type="number" name="prijs" id="prijs" class="form-control" min="0" max="99" step=".01" required>
+            </div>
+            <div class="form-group">
+               <fieldset>
+                  <legend>Beschikbaar als takeaway?</em></legend>
+                  <input type="radio" name="actief" value="1" />
+                  <label for="actief">Ja</label>
+                  <input type="radio" name="actief" value="0" />
+                  <label for="actief">Nee</label>
+               </fieldset>
+            </div>
+            <div class="form-group">
+               <button type="submit" class="btn btn-info">Toevoegen</button>
+            </div>
+         </form>
+      </div>
+   </div>
    <div class="card mt-5">
       <div class="card-header">
          <h2>Gerecht toevoegen</h2>
@@ -142,6 +137,5 @@ if (isset ($_POST['categorie'])  && isset($_POST['gerecht'])  && isset($_POST['p
          </form>
       </div>
    </div>
->>>>>>> 6427720319898bebc9a1472f55b4a58514f6f1aa
 </div>
 <?php require 'includes/footer.php'; ?>
